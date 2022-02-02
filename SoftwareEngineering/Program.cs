49a3566ab30bec8dafd644f188d676aa572db636
Jonathon-Ford <61033062+Jonathon-Ford@users.noreply.github.com>
2022-02-02 16:28:18 -0500

    New project

---
+++ SoftwareEngineering/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SoftwareEngineering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
