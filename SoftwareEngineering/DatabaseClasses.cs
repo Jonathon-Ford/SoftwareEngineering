b39ecc212e1376bb71e5844cfde96258ee447e8d
Jonathon-Ford <JT4d@yamil.com>
2022-03-29 10:45:37 -0400

    Gitting classes for prepared statements up and running

---
+++ SoftwareEngineering/DatabaseClasses.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SoftwareEngineering
+{
+    class Users
+    {
+
+    }
+    class UserRoles
+    {
+
+    }
+    class Reservations
+    {
+
+    }
+    class Payments
+    {
+
+    }
+    class ReservationTypes
+    {
+
+    }
+    class CreditCards
+    {
+
+    }
+    class BaseRates
+    {
+
+    }
+}
